aef72c9216ef3d5c63b7d5d17b079be850444819
Selam25-30 <selamawitbirhanu123@gmail.com>
2022-10-05 14:10:51 -0700

    config

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Create file school
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/holberton',
+}
